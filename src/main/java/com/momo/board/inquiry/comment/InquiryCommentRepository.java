e50d24caa3a9a82f3663340d63a73a9a5eded3d4
woongi24 <wntlsdnd00@naver.com>
2024-08-09 11:37:18 +0900

    삭
    123

--- src/main/java/com/momo/board/inquiry/comment/InquiryCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.momo.board.inquiry.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InquiryCommentRepository extends JpaRepository<InquiryComment, Integer> {
-
-}
