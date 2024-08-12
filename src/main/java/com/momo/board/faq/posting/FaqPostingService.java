566864c31d3ecb08f20ae4f20f68b99a3c6b8e43
Heather440 <cippy1352@gmail.com>
2024-08-12 15:33:22 +0900

    검색추가
    하긴했는데...ㅜㅡㅜ

--- src/main/java/com/momo/board/faq/posting/FaqPostingService.java
+++ src/main/java/com/momo/board/faq/posting/FaqPostingService.java
@@ -9,13 +9,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.momo.DataNotFoundException;
 import com.momo.board.faq.category.FaqCategory;
 import com.momo.member.Member;
-import com.momo.member.MemberRepository;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -37,12 +43,14 @@ public class FaqPostingService {
 		this.faqPostingRepository.save(faq);
 	}
 	
-	public Page<FaqPosting> getList(int page){
+	public Page<FaqPosting> getList(int page, String kw){
 		List<Sort.Order> sorts = new ArrayList<>();
 		sorts.add(Sort.Order.desc("createDate"));
 		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
 			
-		return this.faqPostingRepository.findAll(pageable);
+		Specification<FaqPosting> spec = search(kw);
+		
+		return this.faqPostingRepository.findAll(spec,pageable);
 	}
 	
 	
@@ -76,5 +84,26 @@ public class FaqPostingService {
 		p.setUpdateDate(LocalDateTime.now());
 		this.faqPostingRepository.save(p);
 	}
+	
+	
+	 private Specification<FaqPosting> search(String kw) {
+	        return new Specification   <>() {
+	            private static final long serialVersionUID = 1L;
+	            @Override
+	            public Predicate toPredicate(Root<FaqPosting> faq, CriteriaQuery<?> query, CriteriaBuilder cb) {
+	                query.distinct(true);  // 중복을 제거 
+	                Join<FaqPosting, Member> m1 = faq.join("author", JoinType.LEFT);
+	               // Join<FaqPosting, FaqCategory > c = faq.join("faqList", JoinType.LEFT);
+	              //  Join<FaqCategory, Member> m2 = c.join("author", JoinType.LEFT);
+	                return cb.or(cb.like(faq.get("subject"), "%" + kw + "%"), // 제목 
+	                        cb.like(faq.get("content"), "%" + kw + "%"),      // 내용 
+	                        cb.like(m1.get("membernick"), "%" + kw + "%"));  // 질문 작성자 
+	               //         cb.like(c.get("content"), "%" + kw + "%"));   // 답변 내용 
+	               //         cb.like(m2.get("membernick"), "%" + kw + "%"));   // 답변 작성자 
+	            }
+	        };
+	    }
+	
+	
 	
 }
