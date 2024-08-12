a11beed35fad5acadf4ded1864ecd699a9eed386
timothypark001 <parksc0521@gmail.com>
2024-08-12 15:53:39 +0900

    QNA 검색기능 추가

--- src/main/java/com/momo/board/ask/posting/AskPostingService.java
+++ src/main/java/com/momo/board/ask/posting/AskPostingService.java
@@ -9,11 +9,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.momo.DataNotFoundException;
+import com.momo.board.ask.comment.AskComment;
 import com.momo.member.Member;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -21,11 +29,12 @@ import lombok.RequiredArgsConstructor;
 public class AskPostingService {
 	private final AskPostingRepository askPostingRepository;
 	//질문글 목록 페이지형식 서비스구문
-	public Page<AskPosting> getList(int page){
+	public Page<AskPosting> getList(int page , String kw){
 		List<Sort.Order> sorts = new ArrayList<Sort.Order>();
 		sorts.add(Sort.Order.desc("createDate"));
 		Pageable pageable = PageRequest.of(page ,  10 , Sort.by(sorts));
-		return this.askPostingRepository.findAll(pageable);
+		Specification<AskPosting> spec = search(kw);
+		return this.askPostingRepository.findAll(spec , pageable);
 	}
 	
 	//질문글 조회 서비스구문
@@ -75,5 +84,24 @@ public class AskPostingService {
 		this.askPostingRepository.save(askPosting);
 	}
 	
-	
+	//검색키워드 서비스구문
+	private Specification<AskPosting> search(String kw){
+		return new Specification<>(){
+			private static final long serialVersionUID = 1L;
+			@Override
+			public Predicate toPredicate(Root<AskPosting> ap , CriteriaQuery<?> query
+					, CriteriaBuilder cb) {
+				query.distinct(true);
+				Join<AskPosting , Member> m1 = ap.join("membernick" , JoinType.LEFT);
+				Join<AskPosting , AskComment> ac = ap.join("askCommentList" , JoinType.LEFT);
+				Join<AskComment , Member> m2 = ap.join("membernick" , JoinType.LEFT);
+				
+				return cb.or(cb.like(ap.get("subject"), "%" + kw + "%"),
+							cb.like(ap.get("content"), "%" + kw + "%"),
+							cb.like(m1.get("membernick"), "%" + kw + "%"),
+							cb.like(ac.get("content"), "%" + kw + "%"),
+							cb.like(m2.get("membernick"), "%" + kw + "%"));
+			}
+		};
+	}
 }
