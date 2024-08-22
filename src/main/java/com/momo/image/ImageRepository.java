78db86ec1bb7404ccda3d906523c4290fe1cefd4
woongi24 <wntlsdnd00@naver.com>
2024-08-22 16:00:51 +0900

    멤버테이블 image관계맺기 추가
    관계맺기 구성
    
    Member >> image와 profile을 fk로 갖고있다(양방향 관계맺기)
    Profile >> Member를 fk로 갖고있다
    Image >> Member를 fk로 갖고있다

--- src/main/java/com/momo/image/ImageRepository.java
+++ src/main/java/com/momo/image/ImageRepository.java
@@ -4,11 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.momo.member.profile.Profile;
+import com.momo.member.Member;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
-	
-	
-	Optional<Image> findByProfile(Profile profile);
+	Optional<Image> findByAuthor(Member member);
 }
