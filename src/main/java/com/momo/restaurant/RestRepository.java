60aba4e788559ffb55294896053d4c79b9307d52
Heather440 <cippy1352@gmail.com>
2024-08-23 12:29:28 +0900

    식당검색 추가햇슴
    그런데 데이터가 없어서 검색이 안됨...

--- src/main/java/com/momo/restaurant/RestRepository.java
+++ src/main/java/com/momo/restaurant/RestRepository.java
@@ -1,7 +1,14 @@
 package com.momo.restaurant;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestRepository extends JpaRepository<Restaurant, Integer>{
 
+	
+	Optional< List<Restaurant>> findByCategory(String category); 
+	List<Restaurant> findAll(Specification<Restaurant> spec);
 }
