bf2a52de8d1303c9ccb987d37195027a09c79edb
timothypark001 <parksc0521@gmail.com>
2024-08-28 11:44:25 +0900

    같이먹기 드디어 도입
    아직 초기버전 ㅠ

---
+++ src/main/java/com/momo/restaurant/et/EatTogetherRepository.java
@@ -0,0 +1,9 @@
+package com.momo.restaurant.et;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EatTogetherRepository extends JpaRepository<EatTogether, Integer>{
+
+	
+}
