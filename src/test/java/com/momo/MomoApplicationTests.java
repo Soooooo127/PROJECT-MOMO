f555147612fd13a6cca9cf57c19091330401efe4
Soooooo127 <wltn125@naver.com>
2024-08-28 17:58:09 +0900

    커밋합니다

--- src/test/java/com/momo/MomoApplicationTests.java
+++ src/test/java/com/momo/MomoApplicationTests.java
@@ -1,14 +1,11 @@
 package com.momo;
 
-import java.time.LocalDateTime;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.momo.member.Member;
 import com.momo.member.MemberRepository;
-import com.momo.member.MemberService;
+import com.momo.restaurant.RestService;
 
 @SpringBootTest
 class MomoApplicationTests {
@@ -16,19 +13,28 @@ class MomoApplicationTests {
 	@Autowired
 	private MemberRepository memberRepository;
 	
+	@Autowired
+	private RestService restService;
+	
 	@Test
 	void contextLoads() {
 		
-		Member member = new Member();
+		/*Member member = new Member();
 		member.setMemberid("admin");
 		member.setPassword("12345");
 		member.setMembernick("관리자");
 		member.setEmail("admin@pandaworld.com");
 		member.setCreateDate(LocalDateTime.now());
-		
 		memberRepository.save(member);
 		
 		
+		for(int i = 0 ; i < 5 ; i++) {
+			String name = String.format("테스트가게[%03d]", i);
+			String category = "한식";
+			this.restService.create(name, category);
+		}
+		
+		*/
 	}
 
 }
