fb33eaeea9d4fbab68a3a18d4113b73a770a703e
timothypark001 <parksc0521@gmail.com>
2024-09-06 14:59:05 +0900

    같이먹기 리스트 검색조건 추가

--- src/main/java/com/momo/restaurant/et/EatTogetherRepository.java
+++ src/main/java/com/momo/restaurant/et/EatTogetherRepository.java
@@ -24,5 +24,25 @@ public interface EatTogetherRepository extends JpaRepository<EatTogether, Intege
 	@Query("select et from EatTogether et where et.applymember=:applymember and et.ettitle like %:ettitle%")
 	Page<EatTogether> findByAuthorAndEttitle(@Param(value="applymember")Member author, @Param(value="ettitle")String ettitle, Pageable pageable);
 	
-   
+	//제목으로 검색하는 레파지토리 
+	@Query("select et from EatTogether et where et.ettitle like %:kw% ")
+	Page<EatTogether> findEttitleByKeyword(@Param("kw") String kw , Pageable pageable);
+		
+	//식당 이름을 검색하는 레파지토리
+    @Query("select "
+            + "distinct et "
+            + "from EatTogether et " 
+            + "left outer join Restaurant r on et.rest=r "
+            + "where "
+            + "   r.name like %:kw% ")
+	Page<EatTogether> findNameByKeyword(@Param("kw") String kw , Pageable pageable);
+    
+    //선호 mbti 를 검색하는 레파지토리
+    @Query("select et from EatTogether et where et.prefmbti like %:kw% ")
+    Page<EatTogether> findPrefmbtiByKeyword(@Param("kw") String kw , Pageable pageable);
+    
+    //선호 성별을 검색하는 레파지토리
+    @Query("select et from EatTogether et where et.prefgender like %:kw% ")
+    Page<EatTogether> findPrefgenderByKeyword(@Param("kw") String kw , Pageable pageable);
+    
 }
